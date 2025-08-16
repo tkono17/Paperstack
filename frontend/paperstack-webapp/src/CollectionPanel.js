92f8cfdd36ad858f6619428e566d10e6db436fed
Takanori Kono <takanori.kono@gmail.com>
2025-08-17 00:45:31 +0900

    Update frontend using fetch

--- frontend/paperstack-webapp/src/CollectionPanel.js
+++ frontend/paperstack-webapp/src/CollectionPanel.js
@@ -1,15 +1,24 @@
+import {useState, useEffect} from 'react';
 import './CollectionPanel.css';
 
 function CollectionPanel(props) {
-  const collections = [
-    {
-      name: "Collection A",
-      condition: "name != \"\""
-    },{
-      name: "Collection B",
-      condition: "name != \"\""
-    }
-  ];
+  const [collections, setCollections] = useState([{
+    'name': 'Articles',
+    'op': '==',
+    'fieldName': 'docType',
+    'value': 'Article'
+  }]);
+
+  useEffect(() => {
+    fetch('http://localhost:3100/query/', {method: 'GET', mode: 'cors'})
+    .then(res => res.json())
+    .then(data => {
+      setCollections(data);
+      console.log('Fetched data');
+  })
+    .catch(error => console.log(error))
+  }, []);
+
 
   const items = collections.map(c => <option value={c.name} key={c.name}>{c.name}</option>);
 
--- frontend/paperstack-webapp/src/DocList.js
+++ frontend/paperstack-webapp/src/DocList.js
@@ -17,30 +17,31 @@ function DocList(props) {
     docType: 'eprint'
   }];
 
-  if (props.queryName != lastQuery) {
+  /*if (props.queryName !== lastQuery) {
     console.log('Query updated to ' + props.queryName);
     setLastQuery(props.queryName);
-    if (props.queryName == 'Collection A') {
+    if (props.queryName === 'Collection A') {
       props.documentSelected(documents[0]);
-    } else if (props.queryName == 'Collection B') {
+    } else if (props.queryName === 'Collection B') {
       props.documentSelected(documents[1]);
     } else {
       props.documentSelected(null);
     }
-  }
+  }*/
 
-  const items = documents.map(x => <div className="DocItem" key={x.name}>
-    <div className="row1" key="1">{x.name} ({x.docType})</div>
-    <div className="row2" key="2">{x.authors}</div>
-    <div className="row3" key="3">{x.title}</div>
+  const items = documents.map(x => 
+    <div className="DocItem" key={x.name}>
+      <div className="row1" key="1">{x.name} ({x.docType})</div>
+      <div className="row2" key="2">{x.authors}</div>
+      <div className="row3" key="3">{x.title}</div>
     </div>);
 
   return (
     <div className="DocListPanel">
-    <h2>Document list</h2>
-    <div className="DocList">
-    {items}
-    </div>
+      <h2>Document list</h2>
+      <div className="DocList">
+        {items}
+      </div>
     </div>
   );
 }
