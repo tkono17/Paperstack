3a72be5ccf2c0563ff5de2437b9993fd6a8335d2
Takanori Kono <takanori.kono@gmail.com>
2026-01-17 17:44:05 +0900

    Resolve conflicts

--- backend/src/appbasics/utils/dbAccess.py
+++ backend/src/appbasics/utils/dbAccess.py
@@ -1,47 +1,3 @@
-<<<<<<< HEAD:backend/src/paperstack_server/utils/dbAccess.py
-from sqlmodel import Session, create_engine, SQLModel
-import logging
-
-log = logging.getLogger(__name__)
-
-class DbAccess:
-    sInstance = None
-
-    @classmethod
-    def get(cls, url=None):
-        x = None
-        if url is None:
-            sApp = getApp()
-            url = sApp.settings.sqliteUrl
-        if cls.sInstance is None:
-            log.info(f'  Create DB instance {url}')
-            cls.sInstance = cls(url)
-        x = cls.sInstance
-        return x
-
-    def __init__(self, url):
-        self.url = url
-        self.engine = None
-
-    def connectDb(self):
-        connect_args = {
-            'check_same_thread': False
-        }
-        self.engine = create_engine(self.url, connect_args=connect_args)
-        return self.engine
-
-    def getSession(self):
-        if self.engine is None:
-            self.connectDb()
-        with Session(self.engine) as session:
-            yield session
-
-    def createTables(self):
-        if self.engine is None:
-            self.connectDb()
-        SQLModel.metadata.create_all(self.engine)
-
-=======
 from sqlmodel import Session, create_engine, SQLModel
 import logging
 
@@ -84,5 +40,3 @@ class DbAccess:
         if self.engine is None:
             self.connectDb()
         SQLModel.metadata.create_all(self.engine)
-
->>>>>>> e77b56f8ec3a102cd20e7f214f11504fca852081:backend/src/appbasics/utils/dbAccess.py
